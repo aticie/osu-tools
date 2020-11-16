dcb3529c80474d1bea859382eead6584a9e4e1ef
e.atici <e.atici@vispera.co>
2020-11-16 19:46:32 +0300

    xexxar stop changing osu-tools code please

--- PerformanceCalculator/Profile/ProfileCommand.cs
+++ PerformanceCalculator/Profile/ProfileCommand.cs
@@ -87,9 +87,9 @@ namespace PerformanceCalculator.Profile
                 {
                     Beatmap = working.BeatmapInfo,
                     LocalPP = localPP,
-                    AimPP = categoryAttribs["Total Aim pp"],
-                    TapPP = categoryAttribs["Total Tap pp"],
-                    AccPP = categoryAttribs["Accuracy pp"],
+                    AimPP = categoryAttribs["Aim"],
+                    TapPP = categoryAttribs["Speed"],
+                    AccPP = categoryAttribs["Accuracy"],
                     LivePP = play.pp,
                     Mods = mods.Length > 0 ? mods.Select(m => m.Acronym).Aggregate((c, n) => $"{c}, {n}") : "None"
                 };
